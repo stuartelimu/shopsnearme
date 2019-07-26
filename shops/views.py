c2981c6ba4e045c0cb4e72f8f9af0e120c05da73
ElimuStuart <stuartelimu@gmail.com>
2019-07-26 09:22:38 +0300

    app is now functional

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+latitude = 0.32358400000000004
+longitude = 32.5935104
+
+# user_location = Point(longitude, latitude, srid=4326)
+user_location = fromstr("POINT" + "("+str(longitude)+" "+str(latitude)+")", srid=4326)
+
+
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location',
+    user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'shops/index.html'
